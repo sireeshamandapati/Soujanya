d6fb7de1d1f1fcccc1001d75d2b16d30ab19b853
sireeshamandapati <34599354+sireeshamandapati@users.noreply.github.com>
2017-12-22 20:40:44 +0530

    Update CollatzSequence.java

--- CollatzSequence.java
+++ CollatzSequence.java
@@ -8,19 +8,20 @@ public class CollatzSequence {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("enter the number:");
 		number = sc.nextInt();
+		sc.close();
 		while(number>1)
 		{
 			if(number%2==0)    // if number is even then
 			{
 				number = number/2;
-				System.out.println(+number);
 			}
 			else
 			{
 				// number is odd then
 				number = (3*number)+1;
-				System.out.println(+number);
+				
 			}
+			System.out.printf(" %d ", number);
 		}
 		
 
